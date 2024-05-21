f8e74dea9a54cc7a77842e37cef486c568aff7ea
hossein-ansari <hossein838485@gmail.com>
2024-05-21 18:49:02 +0330

    add aksShop api

--- Routers/productRouter.js
+++ Routers/productRouter.js
@@ -3,5 +3,6 @@ const productRouter = express.Router()
 const productController = require('../controllers/productController')
 
 productRouter.post('/create' , productController.create)
+productRouter.get('/getAll' , productController.getAll)
 
 module.exports = productRouter
--- controllers/productController.js
+++ controllers/productController.js
@@ -10,8 +10,21 @@ exports.create = async (req, res) => {
   const { name, images, tags } = req.body;
   try {
     const products = await productModel.create({ name, images, tags });
-    res.status(200).json({massage : `product ${name} created`,data: products});
+    res
+      .status(200)
+      .json({ massage: `product ${name} created`, data: products });
   } catch (error) {
-    res.status(500).json({ error: "server error",error });
+    res.status(500).json({ error: "server error", error });
+  }
+};
+exports.getAll = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    if (!products) {
+      res.status(404).json({ massage: "not found" });
+    }
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(404).json({ error: "server error", error });
   }
 };
